50bc389043aa41d2e0429155ca743f9b224f769a
Toro <kamper543@gmail.com>
2022-01-15 01:25:57 +0700

    change txt file in to an arraylist

--- Escape192/Assets/Scripts/Controllers/MapController/MapRenderer.cs
+++ Escape192/Assets/Scripts/Controllers/MapController/MapRenderer.cs
@@ -1,18 +1,55 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class MapRenderer : MonoBehaviour {
+public class MapRenderer : MonoBehaviour
+{
     string SceneName;
+    uint width;
+    uint height;
+
+    uint layer;
 
 
-    private void Start() {
+    private void Start()
+    {
         SceneName = SceneManager.GetActiveScene().name;
         // TextAsset asset = (Resources.Load(SceneName)) as TextAsset;
-        TextAsset asset = (Resources.Load("Resources/Scene/test")) as TextAsset;
-        foreach(char c in asset){
-            print(c);
+        TextAsset asset = (Resources.Load("Scene/test")) as TextAsset;
+        string TextFile = asset.ToString();
+
+        string[] Text = TextFile.Split("\n"[0]);
+        width = uint.Parse(Text[0]);
+        height = uint.Parse(Text[1]);
+        layer = uint.Parse(Text[2]);
+        // width * height
+        Text = Text.Skip(3).ToArray();
+        string MapInfo = string.Join("\n", Text);
+        Text = MapInfo.Split("\n"[0]);
+        // TODO: Deal With ArrayList!!!
+        List<List<List<char>>> Layers = new List<List<List<char>>>();
+        for (int i = 0 ; i < layer ; i ++){
+            List<List<char>> Row = new List<List<char>>();
+            for (int j = 0 ; j < height ; j ++){
+                List<char> Column = new List<char>();
+                for (int k = 0 ; k < width ; k ++){
+                    Column.Add(Text[j][k]);
+                }
+                Row.Add(Column);
+            }
+            Layers.Add(Row);
         }
+
+        print(Layers[0][1][3]);
+        // print((string)((ArrayList)Layers[0])[1]);
+
+        // string[] layers = MapInfo.Split("~"[0]);
+        // print(layers[0]);
+        // foreach (string layer in layers)
+        // {
+        //     print(layer);
+        // }
     }
 }
